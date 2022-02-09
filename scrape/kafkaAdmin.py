3e2f5b6839fb8b18c5854d7ae914748f1f563604
Matthew Jensen <matthewljensen@gmail.com>
2022-02-08 23:30:10 -0500

    Created kafka scraping script
    This python script will connect to a kafka instance and pull the brokers, topics, and consumers.

---
+++ scrape/kafkaAdmin.py
@@ -0,0 +1,74 @@
+from kafka import KafkaAdminClient
+
+#print(dir(KafkaAdminClient))
+
+class Consumer:
+    def __init__(self, clientId, state, groupId, memberId,  clientHost, subscriptions, assignment):
+        self.clientId = clientId
+        self.state = state
+        self.groupId = groupId
+        self.memberId = memberId
+        self.clientHost = clientHost
+        self.subscriptions = subscriptions
+        self.assignment = assignment
+    def __str__(self):
+        return f'Client ID: {self.clientId} \n State: {self.state} \n Host: {self.clientHost} \n Subscriptions: {self.subscriptions} \n Group: {self.groupId} \n Member ID: {self.memberId} \n Assignment: {self.assignment}'
+
+# Global variables
+brokers = []
+topics = []
+consumerGroups = []
+consumers = []
+
+# Kafka Admin Client
+admin = KafkaAdminClient(bootstrap_servers='localhost:9092')
+
+
+def updateKafkaArchitecture():
+    global brokers
+    global topics
+    global consumerGroups
+    global consumers
+
+    # Get information on cluster
+    cluster = admin.describe_cluster()
+    for broker in cluster["brokers"]:
+        brokers.append(broker)
+
+
+    # Get all topics
+    topics = admin.list_topics()
+    #for topic in topics:
+        # describe_topic(topic) doesn't give the output I would expect
+        # print(f'{topic}: {admin.describe_topics(topic)}')
+
+
+    # Get all consumer groups
+    consumerGroups = admin.list_consumer_groups()
+    for group in consumerGroups:
+        groupInfo = admin.describe_consumer_groups(group)
+        for group in groupInfo:
+            if group.state == "Stable":
+                for member in group.members:
+                    consumer = Consumer(member.client_id, group.state, group.group, member.member_id, member.client_host, member.member_metadata.subscription, member.member_assignment)
+                    consumers.append(consumer)
+    for consumer in consumers:
+        print(consumer)
+
+def print_kafka_architecture():
+    print('\nBrokers:')
+    for broker in brokers:
+        print(broker)
+    print('\nTopics:')
+    for topic in topics:
+        print(topic)
+    print('\nConsumer Groups:')
+    for group in consumerGroups:
+        print(group)
+    print('\nActive Consumers:')
+    for consumer in consumers:
+        print(consumer)
+        print()
+
+updateKafkaArchitecture()
+print_kafka_architecture()
