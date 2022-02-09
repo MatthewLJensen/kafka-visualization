efd10671f8886dc5f5bb1a0b71541e90d3192b03
Matthew Jensen <matthewljensen@gmail.com>
2022-02-08 23:30:47 -0500

    Adjusted the environment application to have multiple consumers monitoring different topics.

--- environment/consumer.js
+++ environment/consumer.js
@@ -1,25 +1,17 @@
 const { Kafka } = require("kafkajs")
-
-// the client ID lets kafka know who's producing the messages
-const clientId = "web-consumer"
-// we can define the list of brokers in the cluster
 const brokers = ["localhost:9092"]
-// this is the topic from which we want to consume messages
-const topic = "locations"
 
-// initialize a new kafka client and initialize a consumer from it
-const kafka = new Kafka({ clientId, brokers })
-const consumer = kafka.consumer({ groupId: clientId })
 
-const consume = async () => {
-	// first, we wait for the client to connect and subscribe to the given topic
+
+const consume = async (topic, groupId, clientId) => {
+    const kafka = new Kafka({ clientId, brokers })
+    const consumer = kafka.consumer({ groupId: groupId, clientId: clientId })
+
 	await consumer.connect()
 	await consumer.subscribe({ topic })
 	await consumer.run({
-		// this function is called every time the consumer gets a new message
 		eachMessage: ({ message }) => {
-			// here, we just log the message to the standard output
-			console.log(`received message: ${message.value}`)
+			console.log(message.value)
 		},
 	})
 }
--- environment/index.js
+++ environment/index.js
@@ -6,7 +6,16 @@ produce().catch((err) => {
 	console.error("error in producer: ", err)
 })
 
-// start the consumer, and log any errors
-consume().catch((err) => {
+// start the consumers, and log any errors
+// We start 3 consumers, each with a different topic, 2 of them share the same groupId
+consume("locations", "geo", "client1").catch((err) => {
+	console.error("error in consumer: ", err)
+})
+
+consume("access_log", "web", "client2").catch((err) => {
+	console.error("error in consumer: ", err)
+})
+
+consume("error_log", "web", "client3").catch((err) => {
 	console.error("error in consumer: ", err)
 })
