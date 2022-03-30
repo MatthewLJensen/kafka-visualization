3fd6f9323fdf709b3eee1b775210e60c0076513c
Miro Manestar <miroimanestar@gmail.com>
2022-03-30 01:00:07 -0400

    Fixed graph rerender

--- client/src/Viz.jsx
+++ client/src/Viz.jsx
@@ -22,38 +22,53 @@ const Viz = () => {
         let edgesArray = []
         Object.keys(producers).forEach(key => { 
             const producer = producers[key]
-            nodesArray.push({ id: `producer_${producer.id}`, label: producer.id }) 
-            edgesArray.push({ from: `producer_${producer.id}`, to: `topic_${producer.topic}`, arrows: 'to' })
+            nodesArray.push({ id: `producer_${producer.id}`, label: producer.id })
+
+            const edgeId = `producer_${producer.id}-topic_${producer.topic}`
+            edgesArray.push({ id: edgeId, from: `producer_${producer.id}`, to: `topic_${producer.topic}`, arrows: 'to' })
         })
         consumers.forEach(consumer => { 
             nodesArray.push({ id: `consumer_${consumer.consumerId}`, label: consumer.consumerId }) 
 
             consumer.subscriptions.topics.forEach(topic => {
-                edgesArray.push({ from: `consumer_${consumer.consumerId}`, to: `topic_${topic}`, arrows: 'to' })
+                const edgeId = `consumer_${consumer.consumerId}-topic_${topic}`
+                edgesArray.push({ id: edgeId, from: `consumer_${consumer.consumerId}`, to: `topic_${topic}`, arrows: 'to' })
             })
         })
         topics.forEach(topic => nodesArray.push({ id: `topic_${topic}`, label: topic }) )
+        
+        network.current && nodesArray.forEach((node, index) => {
+            const positions = network.current.getPositions()
+            const nodeId = node.id
+            nodesArray[index].x = positions[nodeId]?.x
+            nodesArray[index].y = positions[nodeId]?.y
+        })
 
-        const nodes = new DataSet(nodesArray)
-        const edges = new DataSet(edgesArray)
-
-        const graph = {
-            nodes: nodes, edges: edges
-        }
-
-        const options = { interaction: { zoomView: false } }
-
-        if (!network.current)
+        if (!network.current) {
+            const nodes = new DataSet(nodesArray)
+            const edges = new DataSet(edgesArray)
+    
+            const graph = {
+                nodes: nodes, edges: edges
+            }
+    
+            const options = { 
+                interaction: { zoomView: false },
+                physics: { stabilization: true }
+            }
+            
             network.current = visRef.current && new Network(visRef.current, graph, options)
-        else
-            network.current.setData(graph)
+        } else {
+            network.current.body.data.nodes.update(nodesArray)
+            network.current.body.data.edges.update(edgesArray)
+        }
     }
 
     useEffect(() => {
         window.addEventListener('producersUpdate', startNetwork)
         window.addEventListener('consumersUpdate', startNetwork)
         window.addEventListener('topicsUpdate', startNetwork)
-    }, [visRef])
+    }, [])
 
     return (
         <div id="network" ref={visRef} style={{ width: '100%', height: '600px' }}></div>
